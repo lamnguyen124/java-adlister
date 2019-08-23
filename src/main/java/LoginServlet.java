5cf2d33f582525e51cae9d073255a7fb9e5056cd
Lam Nguyen <nguyen.h.lam12@gmail.com>
2019-08-23 11:05:39 -0500

    update

--- src/main/java/LoginServlet.java
+++ src/main/java/LoginServlet.java
@@ -1,28 +1,36 @@
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
 
-@WebServlet("/login")
-public class LoginServlet {
+@WebServlet(name = "LoginServlet",urlPatterns = "/login")
+public class LoginServlet extends HttpServlet {
 
-    @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException
-        if (!error.equals("") req.setAttribute("error", error);
-    req.getRequestDispatcher("/login.jsp").forward(req,resp);
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
-}
+        request.getRequestDispatcher("/login.jsp").forward(request,response);
+        HttpSession session = request.getSession();
 
-    @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException
-            <%
-            if (request.getMethod().equalsIgnoreCase("post")) {
+//        if(session.getAttribute("username") != null){
+//            boolean isAdmin = (boolean) session.getAttribute("username")
+//        }
+
+    }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
         String username = request.getParameter("username");
         String password = request.getParameter("password");
         if (username.equals("admin") && password.equals("password")) {
             response.sendRedirect("/profile");
         } else {
+            response.sendRedirect("/login?error=Invalid+Credentials");
+        }
+
+
+
     }
-%>
-
-
 }
